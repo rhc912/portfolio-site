dda3848d1cf2e7f2a6dea8e4e618ffa62c7196a4
rhc912 <rhc912@gmail.com>
2016-08-13 09:56:14 -0400

    first commit

---
+++ app/scripts/index.js
@@ -0,0 +1,33 @@
+var $ = require('jquery');
+//Problem: It look gross in smaller browser widths and small devices
+//Solution: To hide the text links and swap them out with a more appropriate navigation
+
+//Create a select and append to #menu
+var $select = $("<select></select>");
+$("#menu").append($select);
+
+//Cycle over menu links
+$("#menu a").each(function(){
+  var $anchor = $(this);
+  //Create an option
+  var $option = $("<option></option>");
+
+  //Deal with selected options depending on current page
+  if($anchor.parent().hasClass("selected")) {
+    $option.prop("selected", true);
+  }
+  //option's value is the href
+  $option.val($anchor.attr("href"));
+  //option's text is the text of link
+  $option.text($anchor.text());
+  //append option to select
+  $select.append($option);
+});
+//Create button
+var $button = $("<button>Go</button>");
+$("#menu")
+//Bind click to button
+$select.change(function(){
+  //Go to select's location
+  window.location = $select.val();
+});
