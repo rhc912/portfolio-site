c54c0392142e35e2ec98af7b1b0b88ad6a955143
rhc912 <rhc912@gmail.com>
2016-08-30 11:47:12 -0400

    more responsive work

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -3,31 +3,31 @@ var $ = require('jquery');
 //Solution: To hide the text links and swap them out with a more appropriate navigation
 
 //Create a select and append to #menu
-var $select = $("<select></select>");
-$("#menu").append($select);
-
-//Cycle over menu links
-$("#menu a").each(function(){
-  var $anchor = $(this);
-  //Create an option
-  var $option = $("<option></option>");
-
-  //Deal with selected options depending on current page
-  if($anchor.parent().hasClass("selected")) {
-    $option.prop("selected", true);
-  }
-  //option's value is the href
-  $option.val($anchor.attr("href"));
-  //option's text is the text of link
-  $option.text($anchor.text());
-  //append option to select
-  $select.append($option);
-});
-//Create button
-var $button = $("<button>Go</button>");
-$("#menu")
-//Bind click to button
-$select.change(function(){
-  //Go to select's location
-  window.location = $select.val();
-});
+// var $select = $("<select></select>");
+// $("#menu").append($select);
+//
+// //Cycle over menu links
+// $("#menu a").each(function(){
+//   var $anchor = $(this);
+//   //Create an option
+//   var $option = $("<option></option>");
+//
+//   //Deal with selected options depending on current page
+//   if($anchor.parent().hasClass("selected")) {
+//     $option.prop("selected", true);
+//   }
+//   //option's value is the href
+//   $option.val($anchor.attr("href"));
+//   //option's text is the text of link
+//   $option.text($anchor.text());
+//   //append option to select
+//   $select.append($option);
+// });
+// //Create button
+// var $button = $("<button>Go</button>");
+// $("#menu")
+// //Bind click to button
+// $select.change(function(){
+//   //Go to select's location
+//   window.location = $select.val();
+// });
